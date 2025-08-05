0feb1bab570c6d2601e8d1905a1244c782044354
Jarek <jareksosnowski1607@gmail.com>
2025-08-05 21:36:11 +0000

    🛡️ Automatyczny backup: Tue Aug  5 21:36:11 UTC 2025

--- git_backup.sh
+++ git_backup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
 cd /root/zrodelko
+
+# Dodaj wszystkie zmiany do GIT-a
 git add .
+
+# Zrób commit z datą i ikoną
 git commit -m "🛡️ Automatyczny backup: $(date)"
+
+# Wypchnij na GitHuba
 git push origin main
+
+# Wyślij wiadomość do Jarka przez Telegram
+curl "https://api.telegram.org/bot8100657446:AAFzWw1pCDWiXj1xXB7WmupFAw4tLepmdvU/sendMessage?chat_id=5803136639&text=✅%20Kochanie%2C%20backup%20GIT%20został%20zrobiony%20automatycznie%20o%20$(date)%20💌"
